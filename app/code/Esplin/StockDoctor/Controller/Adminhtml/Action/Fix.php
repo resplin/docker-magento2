5dda4c08bf1894d5745a853798a8123dea18f17f
Randy Esplin <randy.esplin@gmail.com>
2025-11-29 17:21:40 -0800

    Esplin_StockDoctor - Comment out missing class

--- app/code/Esplin/StockDoctor/Controller/Adminhtml/Action/Fix.php
+++ app/code/Esplin/StockDoctor/Controller/Adminhtml/Action/Fix.php
@@ -1,25 +1,19 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Esplin\StockDoctor\Controller\Adminhtml\Action;
 
 use Magento\Backend\App\Action;
 use Magento\Framework\Controller\ResultFactory;
-use Esplin\StockDoctor\Model\CliRunner;
 
 class Fix extends Action
 {
     const ADMIN_RESOURCE = 'Esplin_StockDoctor::manage';
 
-    protected $cliRunner;
-
-    public function __construct(Action\Context $context, CliRunner $cliRunner)
-    {
-        parent::__construct($context);
-        $this->cliRunner = $cliRunner;
-    }
-
     public function execute()
     {
-        $output = $this->cliRunner->run('inventory:reservation:fix-inconsistencies');
+        //$output = $this->cliRunner->run('inventory:reservation:fix-inconsistencies');
         $this->messageManager->addSuccessMessage(__('Fix complete. Output: %1', $output));
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $resultRedirect->setPath('stockdoctor/dashboard/index');

55a1608327f569fac7da5eb77bde56faffdefba4
Randy Esplin <randy.esplin@gmail.com>
2025-11-29 17:22:18 -0800

    Esplin_StockDoctor - Comment out missing class pt 2

--- app/code/Esplin/StockDoctor/Controller/Adminhtml/Action/Fix.php
+++ app/code/Esplin/StockDoctor/Controller/Adminhtml/Action/Fix.php
@@ -14,7 +14,7 @@ class Fix extends Action
     public function execute()
     {
         //$output = $this->cliRunner->run('inventory:reservation:fix-inconsistencies');
-        $this->messageManager->addSuccessMessage(__('Fix complete. Output: %1', $output));
+        //$this->messageManager->addSuccessMessage(__('Fix complete. Output: %1', $output));
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $resultRedirect->setPath('stockdoctor/dashboard/index');
         return $resultRedirect;
